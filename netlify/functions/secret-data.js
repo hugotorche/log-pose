4cbb04b89d6313d1f04a24290fcd37a0acb44f47
Hugo Torche <hugo.torche@kedgebs.com>
2025-08-13 23:11:56 +0200

    adding node express backend

--- netlify/functions/secret-data.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async function(event, context) {
-    const MAP_TILE_TOKEN = process.env.MAP_TILE_TOKEN; // Securely access your secret key
-  
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Response from Netlify function with secrets" }),
-    };
-  };
