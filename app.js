cf5e88aca3041e5dae10bb222e3110859f0e55ea
Hugo Torche <hugo.torche@kedgebs.com>
2025-08-31 13:31:33 +0200

    adding navbar resume

--- app.js
+++ app.js
@@ -35,6 +35,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/resume', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'Resume.pdf'));
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
